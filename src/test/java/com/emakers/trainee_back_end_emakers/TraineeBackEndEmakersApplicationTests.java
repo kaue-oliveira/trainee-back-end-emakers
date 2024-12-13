b68b910280e27b3ddb2a39a866d4b53c8feda6c4
kaue-oliveira <kaueolivsil@gmail.com>
2024-12-13 14:51:04 -0300

    Criando projeto spring boot

---
+++ src/test/java/com/emakers/trainee_back_end_emakers/TraineeBackEndEmakersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emakers.trainee_back_end_emakers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraineeBackEndEmakersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
